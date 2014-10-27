b7883e83134a2a05bc927dbfb56419013263fd6d
Alexej Yaroshevich <alex@qfox.ru>
2014-10-27 13:50:57 +0400

    initial

---
+++ src/Parser.php
@@ -0,0 +1,671 @@
+<?php
+
+namespace CSSSelectorParser;
+
+use \Exception;
+
+function __isIdentStart ($c) {
+	return ctype_alpha($c);
+}
+
+function __isIdent ($c) {
+	return ctype_alnum($c) || $c == '-' || $c == '_';
+}
+
+function __isHex ($c) {
+	return ctype_xdigit($c);
+}
+
+function __isAttrMatchOperator ($c) {
+	return $c == '=' || $c == '^' || $c == '$' || $c == '*' || $c == '~';
+}
+
+if (!function_exists('mb_chr')) {
+    function mb_chr($ord, $encoding = 'UTF-8') {
+        if ($encoding === 'UCS-4BE') {
+            return pack("N", $ord);
+        }
+        else {
+            return mb_convert_encoding(mb_chr($ord, 'UCS-4BE'), $encoding, 'UCS-4BE');
+        }
+    }
+}
+
+if (!function_exists('mb_ord')) {
+    function mb_ord($char, $encoding = 'UTF-8') {
+        if ($encoding === 'UCS-4BE') {
+            list(, $ord) = (strlen($char) === 4) ? @unpack('N', $char) : @unpack('n', $char);
+            return $ord;
+        }
+        else {
+            return mb_ord(mb_convert_encoding($char, 'UCS-4BE', $encoding), 'UCS-4BE');
+        }
+    }
+}
+
+class Context {
+	protected $str;
+	protected $l = 0;
+	protected $pos = 0;
+	protected $ch;
+
+	protected $pseudos = [];
+	protected $attrEqualityMods = [];
+	protected $ruleNestingOperators = [];
+	protected $substitutesEnabled = false;
+	protected $identSpecialChars = [];
+	protected $identReplacements = [];
+	protected $identReplacementsRev = [];
+	protected $strReplacementsRev = [];
+	protected $singleQuoteEscapeChars = [];
+	protected $doubleQuotesEscapeChars = [];
+
+	public function __construct ($str, $pos, $conf) {
+		$this->str = $str;
+		$this->l = strlen($str);
+		$this->pos = $pos;
+
+		// parser
+		foreach ($conf as $k => $v) {
+			$this->$k = $v;
+		}
+
+		$this->nextCh(false);
+	}
+
+	protected function nextCh ($inc = true) {
+		if ($inc) {
+			$this->pos++;
+		}
+		$this->ch = isset($this->str[$this->pos]) ? $this->str[$this->pos] : null;
+	}
+
+	protected function getStr ($quote, $escapeTable) {
+		$result = '';
+		$this->nextCh();
+		while ($this->pos < $this->l) {
+			if ($this->ch == $quote) {
+				$this->pos++;
+				return $result;
+			}
+			elseif ($this->ch == '\\') {
+				$this->nextCh();
+				if ($this->ch == $quote) {
+					$result .= $quote;
+				}
+				elseif (isset($escapeTable[$this->ch])) {
+					$result .= $escapeTable[$this->ch];
+				}
+				elseif (__isHex($this->ch)) {
+					$hex = $this->ch;
+					$this->nextCh();
+					while (__isHex($this->ch)) {
+						$hex .= $this->ch;
+						$this->nextCh();
+					}
+					if ($this->ch == ' ') {
+						$this->nextCh();
+					}
+					$result .= mb_chr(hexdec($hex));
+					continue;
+				}
+				else {
+					$result .= $this->ch;
+				}
+			}
+			else {
+				$result .= $this->ch;
+			}
+			$this->nextCh();
+		}
+		return $result;
+	}
+
+	protected function getIdent () {
+		$result = '';
+		$this->nextCh(false);
+		while ($this->pos < $this->l) {
+			if (__isIdent($this->ch)) {
+				$result .= $this->ch;
+			}
+			elseif ($this->ch == '\\') {
+				$this->nextCh();
+				if (isset($this->identSpecialChars[$this->ch])) {
+					$result .= $this->ch;
+				}
+				elseif (isset($this->identReplacements[$this->ch])) {
+					$result .= $this->identReplacements[$this->ch];
+				}
+				elseif (__isHex($this->ch)) {
+					$hex = $this->ch;
+					$this->nextCh();
+					while (__isHex($this->ch)) {
+						$hex .= $this->ch;
+						$this->nextCh();
+					}
+					if ($this->ch == ' ') {
+						$this->nextCh();
+					}
+					$result .= mb_chr(hexdec($hex));
+					continue;
+				}
+				else {
+					$result .= $this->ch;
+				}
+			}
+			else {
+				return $result;
+			}
+			$this->nextCh();
+		}
+		return $result;
+	}
+
+	protected function skipWhitespace () {
+		$this->nextCh(false);
+		$result = false;
+		while ($this->ch == ' ' || $this->ch == "\t" || $this->ch == "\n" || $this->ch == "\r" || $this->ch == "\f") {
+			$result = true;
+			$this->nextCh();
+		}
+		return $result;
+	}
+
+	// @parse = ->
+	public function parse () {
+		$res = $this->parseSelector();
+		if ($this->pos < $this->l) {
+			throw new Exception('Rule expected but "' . $this->str[$this->pos] . '" found.');
+		}
+		return $res;
+	}
+
+	// @parseSelector = ->
+	protected function parseSelector () {
+		$selector = $res = $this->parseSingleSelector();
+		$this->nextCh(false);
+		while ($this->ch == ',') {
+			$this->pos++;
+			$this->skipWhitespace();
+			if ($res['type'] != 'selectors') {
+				$res = [
+					'type' => 'selectors',
+					'selectors' => [$selector]
+				];
+			}
+			$selector = $this->parseSingleSelector();
+			if (!$selector) {
+				throw new Exception('Rule expected after ",".');
+			}
+			$res['selectors'][] = $selector;
+		}
+		return $res;
+	}
+
+	// @parseSingleSelector = ->
+	protected function parseSingleSelector () {
+		$this->skipWhitespace();
+		$selector = ['type' => 'ruleSet'];
+		$rule = $this->parseRule();
+		if (!$rule) {
+			return null;
+		}
+
+		$currentRule =& $selector;
+		while ($rule) {
+			$rule['type'] = 'rule';
+			$currentRule['rule'] = $rule;
+			$currentRule =& $currentRule['rule'];
+
+			$this->skipWhitespace();
+			$this->nextCh(false);
+			if ($this->pos >= $this->l || $this->ch == ',' || $this->ch == ')') {
+				break;
+			}
+			if (isset($this->ruleNestingOperators[$this->ch])) {
+				$op = $this->ch;
+				$this->pos++;
+				$this->skipWhitespace();
+				$rule = $this->parseRule();
+				if (!$rule) {
+					throw new Exception('Rule expected after "' . $op . '".');
+				}
+				$rule['nestingOperator'] = $op;
+			}
+			else {
+				$rule = $this->parseRule();
+				if ($rule) {
+					$rule['nestingOperator'] = null;
+				}
+			}
+		}
+		return $selector;
+	}
+
+	function parseRule () {
+		$rule = null;
+		while ($this->pos < $this->l) {
+			$this->nextCh(false);
+			if ($this->ch == '*') {
+				$rule = $rule ?: [];
+				$rule['tagName'] = '*';
+			}
+			elseif (__isIdentStart($this->ch) || $this->ch == '\\') {
+				$rule = $rule ?: [];
+				$rule['tagName'] = $this->getIdent();
+			}
+			elseif ($this->ch == '.') {
+				$this->pos++;
+				$rule = $rule ?: [];
+				$rule['classNames'] = @$rule['classNames'] ?: [];
+				$rule['classNames'][] = $this->getIdent();
+			}
+			elseif ($this->ch == '#') {
+				$this->nextCh();
+				$id = '';
+				while (__isIdent($this->ch)) {
+					$id .= $this->ch;
+					$this->nextCh();
+				}
+				$rule = $rule ?: [];
+				$rule['id'] = $id;
+			}
+			elseif ($this->ch == '[') {
+				$this->pos++;
+				$this->skipWhitespace();
+				$attr = ['name' => $this->getIdent()];
+				$this->skipWhitespace();
+				if ($this->ch == ']') {
+					$this->pos++;
+				}
+				else {
+					$operator = '';
+					if (isset($this->attrEqualityMods[$this->ch])) {
+						$operator = $this->ch;
+						$this->nextCh();
+					}
+					if ($this->pos >= $this->l) {
+						throw new Exception('Expected "=" but end of file reached.');
+					}
+					if ($this->ch != '=') {
+						throw new Exception('Expected "=" but "' . $this->ch . '" found.');
+					}
+					$attr['operator'] = $operator . '=';
+					$this->pos++;
+					$this->skipWhitespace();
+					$attrValue = '';
+					$attr['valueType'] = 'string';
+					if ($this->ch == '"') {
+						$attrValue = $this->getStr('"', $this->doubleQuotesEscapeChars);
+					}
+					elseif ($this->ch == '\'') {
+						$attrValue = $this->getStr('\'', $this->singleQuoteEscapeChars);
+					}
+					elseif ($this->substitutesEnabled && $this->ch == '$') {
+						$this->pos++;
+						$attrValue = $this->getIdent();
+						$attr['valueType'] = 'substitute';
+					}
+					else {
+						while ($this->pos < $this->l) {
+							if ($this->ch == ']') {
+								break;
+							}
+							$attrValue .= $this->ch;
+							$this->nextCh();
+						}
+						$attrValue = trim($attrValue);
+					}
+					$this->skipWhitespace();
+					if ($this->pos >= $this->l) {
+						throw new Exception('Expected "]" but end of file reached.');
+					}
+					if ($this->ch != ']') {
+						throw new Exception('Expected "]" but "' . $this->ch . '" found.');
+					}
+					$this->pos++;
+					$attr['value'] = $attrValue;
+				}
+				$rule = $rule ?: [];
+				$rule['attrs'] = @$rule['attrs'] ?: [];
+				$rule['attrs'][] = $attr;
+			}
+			elseif ($this->ch == ':') {
+				$this->pos++;
+				$pseudoName = $this->getIdent();
+				$pseudo = ['name' => $pseudoName];
+				if ($this->ch == '(') {
+					$this->pos++;
+					$value = '';
+					$this->skipWhitespace();
+					if (isset($this->pseudos[$pseudoName]) && $this->pseudos[$pseudoName] == 'selector') {
+						$pseudo['valueType'] = 'selector';
+						$value = $this->parseSelector();
+					}
+					else {
+						$pseudo['valueType'] = 'string';
+						if ($this->ch == '"') {
+							$value = $this->getStr('"', $this->doubleQuotesEscapeChars);
+						}
+						elseif ($this->ch == '\'') {
+							$value = $this->getStr('\'', $this->singleQuoteEscapeChars);
+						}
+						elseif ($this->substitutesEnabled && $this->ch == '$') {
+							$this->pos++;
+							$value = $this->getIdent();
+							$pseudo['valueType'] = 'substitute';
+						}
+						else {
+							while ($this->pos < $this->l) {
+								if ($this->ch == ')') {
+									break;
+								}
+								$value .= $this->ch;
+								$this->nextCh();
+							}
+							$value = trim($value);
+						}
+						$this->skipWhitespace();
+					}
+					if ($this->pos >= $this->l) {
+						throw new Exception('Expected ")" but end of file reached.');
+					}
+					if ($this->ch != ')') {
+						throw new Exception('Expected ")" but "' . $this->ch . '" found.');
+					}
+					$this->pos++;
+					$pseudo['value'] = $value;
+				}
+				$rule = $rule ?: [];
+				$rule['pseudos'] = @$rule['pseudos'] ?: [];
+				$rule['pseudos'][] = $pseudo;
+			}
+			else {
+				break;
+			}
+		}
+		return $rule;
+	}
+}
+
+class Parser {
+	protected $pseudos = [];
+	protected $attrEqualityMods = [];
+	protected $ruleNestingOperators = [];
+	protected $substitutesEnabled = false;
+
+	private $identSpecialChars = [
+		'!'  => true,
+		'"'  => true,
+		'#'  => true,
+		'$'  => true,
+		'%'  => true,
+		'&'  => true,
+		'\'' => true,
+		'('  => true,
+		')'  => true,
+		'*'  => true,
+		'+'  => true,
+		','  => true,
+		'.'  => true,
+		'/'  => true,
+		';'  => true,
+		'<'  => true,
+		'='  => true,
+		'>'  => true,
+		'?'  => true,
+		'@'  => true,
+		'['  => true,
+		'\\' => true,
+		']'  => true,
+		'^'  => true,
+		'`'  => true,
+		'{'  => true,
+		'|'  => true,
+		'}'  => true,
+		'~'  => true,
+	];
+
+	private $identReplacements = [
+		"n"  => "\n",
+		"r"  => "\r",
+		"t"  => "\t",
+		" "  => " ",
+		"f"  => "\f",
+		"v"  => "\v",
+	];
+
+	private $identReplacementsRev = [
+		"\n" => "\\n",
+		"\r" => "\\r",
+		"\t" => "\\t",
+		" "  => "\\ ",
+		"\f" => "\\f",
+		"\v" => "\\v",
+	];
+
+	private $strReplacementsRev = [
+		"\n" => "\\n",
+		"\r" => "\\r",
+		"\t" => "\\t",
+		"\f" => "\\f",
+		"\v" => "\\v",
+	];
+
+	private $singleQuoteEscapeChars = [
+		"n"  => "\n",
+		"r"  => "\r",
+		"t"  => "\t",
+		"f"  => "\f",
+		"\\" => "\\",
+		"\'" => "\'",
+	];
+
+	private $doubleQuotesEscapeChars = [
+		"n"  => "\n",
+		"r"  => "\r",
+		"t"  => "\t",
+		"f"  => "\f",
+		"\\" => "\\",
+		"\"" => "\"",
+	];
+
+	public function __construct () {
+	}
+
+	public function registerSelectorPseudos () {
+		$arguments = func_get_args();
+		foreach ($arguments as $name) {
+			$this->pseudos[$name] = 'selector';
+		}
+		return $this;
+	}
+
+	public function unregisterSelectorPseudos () {
+		$arguments = func_get_args();
+		foreach ($arguments as $name) {
+			unset($this->pseudos[$name]);
+		}
+		return $this;
+	}
+
+	public function registerNestingOperators () {
+		$arguments = func_get_args();
+		foreach ($arguments as $op) {
+			$this->ruleNestingOperators[$op] = true;
+		}
+		return $this;
+	}
+
+	public function unregisterNestingOperators () {
+		$arguments = func_get_args();
+		foreach ($arguments as $op) {
+			unset($this->ruleNestingOperators[$op]);
+		}
+		return $this;
+	}
+
+	public function registerAttrEqualityMods () {
+		$arguments = func_get_args();
+		foreach ($arguments as $mod) {
+			$this->attrEqualityMods[$mod] = true;
+		}
+		return $this;
+	}
+
+	public function unregisterAttrEqualityMods () {
+		$arguments = func_get_args();
+		foreach ($arguments as $mod) {
+			unset($this->attrEqualityMods[$mod]);
+		}
+		return $this;
+	}
+
+	public function enableSubstitutes () {
+		$this->substitutesEnabled = true;
+		return $this;
+	}
+	public function disableSubstitutes () {
+		$this->substitutesEnabled = false;
+		return $this;
+	}
+
+	public function parse ($str) {
+		$conf = [
+			'pseudos'                 => $this->pseudos,
+			'attrEqualityMods'        => $this->attrEqualityMods,
+			'ruleNestingOperators'    => $this->ruleNestingOperators,
+			'substitutesEnabled'      => $this->substitutesEnabled,
+			'identSpecialChars'       => $this->identSpecialChars,
+			'identReplacements'       => $this->identReplacements,
+			'identReplacementsRev'    => $this->identReplacementsRev,
+			'strReplacementsRev'      => $this->strReplacementsRev,
+			'singleQuoteEscapeChars'  => $this->singleQuoteEscapeChars,
+			'doubleQuotesEscapeChars' => $this->doubleQuotesEscapeChars,
+		];
+		$ctx = new Context($str, 0, $conf);
+		return $ctx->parse();
+	}
+
+	public function render ($path) {
+		$renderEntity = function ($entity) use (&$renderEntity) {
+			$res = '';
+			switch ($entity['type']) {
+				case 'ruleSet':
+					$currentEntity = $entity['rule'];
+					$parts = [];
+					while ($currentEntity) {
+						if (isset($currentEntity['nestingOperator'])) {
+							$parts[] = $currentEntity['nestingOperator'];
+						}
+						$parts[] = $renderEntity($currentEntity);
+						$currentEntity = isset($currentEntity['rule']) ? $currentEntity['rule'] : null;
+					}
+					$res = join(' ', $parts);
+					break;
+
+				case 'selectors':
+					$res = join(', ', array_map($renderEntity, $entity['selectors']));
+					break;
+
+				case 'rule':
+					if (isset($entity['tagName'])) {
+						$res = $this->escapeIdentifier($entity['tagName']);
+					}
+					if (isset($entity['id'])) {
+						$res .= '#' . $this->escapeIdentifier($entity['id']);
+					}
+					if (isset($entity['classNames'])) {
+						$that = $this;
+						$res .= join('', array_map(function ($cn) use ($that) {
+							return '.' . $that->escapeIdentifier($cn);
+						}, $entity['classNames']));
+					}
+					if (isset($entity['attrs'])) {
+						$res .= join('', array_map(function ($attr) {
+							if (isset($attr['operator'])) {
+								if ($attr['valueType'] == 'substitute') {
+									return "[{$this->escapeIdentifier($attr['name'])}{$attr['operator']}\${$attr['value']}]";
+								}
+								else {
+									return "[{$this->escapeIdentifier($attr['name'])}{$attr['operator']}{$this->escapeStr($attr['value'])}]";
+								}
+							}
+							else {
+								return "[{$this->escapeIdentifier($attr['name'])}]";
+							}
+						}, $entity['attrs']));
+					}
+					if (isset($entity['pseudos'])) {
+						$res .= join('', array_map(function ($pseudo) use (&$renderEntity) {
+							if (isset($pseudo['valueType'])) {
+								if ($pseudo['valueType'] == 'selector') {
+									return ":{$this->escapeIdentifier($pseudo['name'])}({$renderEntity($pseudo['value'])})";
+								}
+								elseif ($pseudo['valueType'] == 'substitute') {
+									return ":{$this->escapeIdentifier($pseudo['name'])}(\${$pseudo['value']})";
+								}
+								else {
+									return ":{$this->escapeIdentifier($pseudo['name'])}({$this->escapeStr($pseudo['value'])})";
+								}
+							}
+							else {
+								return ":{$this->escapeIdentifier($pseudo['name'])}";
+							}
+						}, $entity['pseudos']));
+					}
+					break;
+				default:
+					throw new Exception('Unknown entity type: "' . $entity['type'] . '".');
+			}
+			return $res;
+		};
+		$renderEntity->bindTo($this);
+
+		return $renderEntity($path);
+	}
+
+	protected function escapeIdentifier ($s) {
+		$result = '';
+		$i = 0;
+		$l = mb_strlen($s);
+		while ($i < $l) {
+			$c = mb_substr($s, $i, 1);
+			if (isset($this->identSpecialChars[$c])) {
+				$result .= '\\' . $c;
+			}
+			elseif (isset($this->identReplacementsRev[$c])) {
+				$result .= $this->identReplacementsRev[$c];
+			}
+			elseif (($cc = mb_ord($c)) && ($cc < 32 || $cc > 126)) {
+				$result .= '\\' . dechex($cc) . ' ';
+			}
+			else {
+				$result .= $c;
+			}
+			$i++;
+		}
+		return $result;
+	}
+
+	public function escapeStr ($s) {
+		$result = '';
+		$i = 0;
+		$l = mb_strlen($s);
+		while ($i < $l) {
+			$c = mb_substr($s, $i, 1);
+			if ($c == '"') {
+				$c = '\\"';
+			}
+			elseif ($c == '\\') {
+				$c = '\\\\';
+			}
+			elseif (isset($this->strReplacementsRev[$c])) {
+				$c = $this->strReplacementsRev[$c];
+			}
+			$result .= $c;
+			$i++;
+		}
+		return "\"{$result}\"";
+	}
+}
---
+++ tests/Parser.Test.php
@@ -0,0 +1,200 @@
+<?php
+
+use CSSSelectorParser\Parser;
+
+class ParserTest extends PHPUnit_Framework_TestCase {
+
+	function setUp () {
+		$this->parser = new Parser();
+		$this->parser->registerAttrEqualityMods('^', '$', '*', '~');
+		$this->parser->registerNestingOperators('>', '+', '~');
+	}
+
+	function testClassParsing () {
+		$this->assertEquals('.class', $this->parser->render($this->parser->parse('.class')));
+		$this->assertEquals('.class1.class2', $this->parser->render($this->parser->parse('.class1.class2')));
+	}
+
+	function testTagIdParsing () {
+		$this->assertEquals('tag.class', $this->parser->render($this->parser->parse('tag.class')));
+		$this->assertEquals('tag#id.class', $this->parser->render($this->parser->parse('tag#id.class')));
+	}
+
+	function testAttrParsing () {
+		$this->assertEquals('tag#id.class[attr]', $this->parser->render($this->parser->parse('tag#id.class[attr]')));
+		$this->assertEquals('tag#id.class[attr]', $this->parser->render($this->parser->parse('tag#id.class[ attr ]')));
+		$this->assertEquals('tag#id.class[attr="value"]', $this->parser->render($this->parser->parse('tag#id.class[attr=value]')));
+		$this->assertEquals('tag#id.class[attr~="value"]', $this->parser->render($this->parser->parse('tag#id.class[attr~=value]')));
+		$this->assertEquals('tag#id.class[attr*="value"]', $this->parser->render($this->parser->parse('tag#id.class[attr*=value]')));
+		$this->assertEquals('tag#id.class[attr^="value"]', $this->parser->render($this->parser->parse('tag#id.class[attr^=value]')));
+		$this->assertEquals('tag#id.class[attr$="value"]', $this->parser->render($this->parser->parse('tag#id.class[attr$=value]')));
+	}
+
+	function testMoreAttrParsing () {
+		$this->assertEquals('tagname[x="y"]', $this->parser->render($this->parser->parse('tagname[   x =  y  ]')));
+		$this->assertEquals('tagname[x="y"]', $this->parser->render($this->parser->parse('tagname[x="y"]')));
+		$this->assertEquals('tagname[x="y"][z]', $this->parser->render($this->parser->parse('tagname[x="y"][z]')));
+		$this->assertEquals('tagname[x="y "]', $this->parser->render($this->parser->parse('tagname[x="y "]')));
+		$this->assertEquals('tagname[x="y \\""]', $this->parser->render($this->parser->parse('tagname[x="y \\""]')));
+		$this->assertEquals('tagname[x="y\'"]', $this->parser->render($this->parser->parse('tagname[x="y\'"]')));
+	}
+
+	function testNestedParsing () {
+		$this->assertEquals('tag1 tag2', $this->parser->render($this->parser->parse('tag1   tag2')));
+		$this->assertEquals('tag1 > tag2', $this->parser->render($this->parser->parse('tag1>tag2')));
+		$this->assertEquals('tag1 + tag2', $this->parser->render($this->parser->parse('tag1+tag2')));
+		$this->assertEquals('tag1 ~ tag2', $this->parser->render($this->parser->parse('tag1~tag2')));
+	}
+
+	function testSimplePseudosParsing () {
+		$this->assertEquals('tag1:first', $this->parser->render($this->parser->parse('tag1:first')));
+		$this->assertEquals('tag1:lt("3")', $this->parser->render($this->parser->parse('tag1:lt(3)')));
+		$this->assertEquals('tag1:lt("3")', $this->parser->render($this->parser->parse('tag1:lt( 3 )')));
+		$this->assertEquals('tag1:lt("3")', $this->parser->render($this->parser->parse('tag1:lt(\'3\')')));
+		$this->assertEquals('tag1:lt("3")', $this->parser->render($this->parser->parse('tag1:lt("3" )')));
+
+	}
+
+	function testAdvancedPseudosParsing () {
+		$this->assertEquals('tag1:has(".class")', $this->parser->render($this->parser->parse('tag1:has(.class)')));
+
+		$this->parser->registerSelectorPseudos('has');
+		$this->assertEquals('tag1:has(.class)', $this->parser->render($this->parser->parse('tag1:has(.class)')));
+		$this->assertEquals('tag1:has(.class, .class2)',
+			$this->parser->render($this->parser->parse('tag1:has(.class,.class2)')));
+		$this->assertEquals('tag1:has(.class:has(.subcls), .class2)',
+			$this->parser->render($this->parser->parse('tag1:has(.class:has(.subcls),.class2)')));
+	}
+
+	function testInvalidPseudo1 () {
+		$this->setExpectedException('Exception', 'Expected ")" but end of file reached.');
+		$this->parser->parse(':has(.class');
+	}
+
+	function testInvalidPseudo2 () {
+		$this->setExpectedException('Exception', 'Expected ")" but end of file reached.');
+		$this->parser->parse(':has(:has(');
+	}
+
+	function testUnregisteringPseudos () {
+		$this->parser->unregisterSelectorPseudos('has');
+		$this->assertEquals('tag1:has(".class,.class2")',
+			$this->parser->render($this->parser->parse('tag1:has(.class,.class2)')));
+	}
+
+
+	function testException1 () {
+		$this->setExpectedException('Exception', 'Expected "]" but "!" found.');
+		$this->parser->parse('[attr="val"!');
+	}
+	function testException2 () {
+		$this->setExpectedException('Exception', 'Expected "]" but end of file reached.');
+		$this->parser->parse('[attr="val"');
+	}
+	function testException3 () {
+		$this->setExpectedException('Exception', 'Expected "=" but "!" found.');
+		$this->parser->parse('[attr!="val"]');
+	}
+	function testException4 () {
+		$this->setExpectedException('Exception', 'Expected "=" but end of file reached.');
+		$this->parser->parse('[attr');
+	}
+	function testException5 () {
+		$this->setExpectedException('Exception', 'Expected ")" but "!" found.');
+		$this->parser->parse(':pseudoName("pseudoValue"!');
+	}
+	function testException6 () {
+		$this->setExpectedException('Exception', 'Expected ")" but end of file reached.');
+		$this->parser->parse(':pseudoName("pseudoValue"');
+	}
+	function testException7 () {
+		$this->setExpectedException('Exception', 'Rule expected after ">".');
+		$this->parser->parse('tag>');
+	}
+	function testException8 () {
+		$this->setExpectedException('Exception', 'Rule expected after "+".');
+		$this->parser->parse('tag+');
+	}
+	function testException9 () {
+		$this->setExpectedException('Exception', 'Rule expected after "~".');
+		$this->parser->parse('tag~');
+	}
+	function testException10 () {
+		$this->setExpectedException('Exception', 'Rule expected but "!" found.');
+		$this->parser->parse('tag !');
+	}
+	function testException11 () {
+		$this->setExpectedException('Exception', 'Rule expected but "!" found.');
+		$this->parser->parse('tag!');
+	}
+	function testException12 () {
+		$this->setExpectedException('Exception', 'Rule expected after ",".');
+		$this->parser->parse('tag,');
+	}
+
+	function testEscaping () {
+		$this->assertEquals('tag\\n\\\\name\\.\\[',
+			$this->parser->render($this->parser->parse('tag\\n\\\\name\\.\\[')));
+		$this->assertEquals('.cls\\n\\\\name\\.\\[',
+			$this->parser->render($this->parser->parse('.cls\\n\\\\name\\.\\[')));
+		$this->assertEquals('[attr\\n\\\\name\\.\\[="1"]',
+			$this->parser->render($this->parser->parse('[attr\\n\\\\name\\.\\[=1]')));
+		$this->assertEquals(':pseudo\\n\\\\name\\.\\[\\(("123")',
+			$this->parser->render($this->parser->parse(':pseudo\\n\\\\name\\.\\[\\((123)')));
+		$this->assertEquals('[attr="val\\nval"]',
+			$this->parser->render($this->parser->parse('[attr="val\nval"]')));
+		$this->assertEquals('[attr="val\\"val"]',
+			$this->parser->render($this->parser->parse('[attr="val\\"val"]')));
+		$this->assertEquals('[attr="val val"]',
+			$this->parser->render($this->parser->parse('[attr="val\\00a0val"]')));
+
+		// skipped cuz bugs:
+		// assertEquals('tag\\a0 tag', $parser->render($parser->parse('tag\\00a0 tag')));
+		// assertEquals('.class\\a0 class', $parser->render($parser->parse('.class\\00a0 class')));
+		// assertEquals('[attr\\a0 attr]', $parser->render($parser->parse('[attr\\a0 attr]')));
+	}
+
+	function testSubstitutes () {
+		$this->assertEquals('[attr="$var"]',
+			$this->parser->render($this->parser->parse('[attr=$var]')));
+		$this->assertEquals(':has("$var")',
+			$this->parser->render($this->parser->parse(':has($var)')));
+
+		$this->parser->enableSubstitutes();
+		$this->assertEquals('[attr=$var]',
+			$this->parser->render($this->parser->parse('[attr=$var]')));
+		$this->assertEquals(':has($var)',
+			$this->parser->render($this->parser->parse(':has($var)')));
+
+		$this->parser->disableSubstitutes();
+		$this->assertEquals('[attr="$var"]',
+			$this->parser->render($this->parser->parse('[attr=$var]')));
+		$this->assertEquals(':has("$var")',
+			$this->parser->render($this->parser->parse(':has($var)')));
+	}
+
+	function testNestingOperators () {
+		$this->parser->registerNestingOperators(';');
+		$this->assertEquals('tag1 ; tag2',
+			$this->parser->render($this->parser->parse('tag1 ; tag2')));
+		$this->parser->unregisterNestingOperators(';');
+
+		$this->setExpectedException('Exception', 'Rule expected but ";" found.');
+		$this->parser->parse('tag1 ; tag2');
+	}
+
+	function testAttrEqualityMods () {
+		$this->parser->registerAttrEqualityMods(';');
+		$this->assertEquals('[attr;="val"]',
+			$this->parser->render($this->parser->parse('[attr;=val]')));
+		$this->parser->unregisterAttrEqualityMods(';');
+
+		$this->setExpectedException('Exception', 'Expected "=" but ";" found.');
+		$this->parser->parse('[attr;=val]');
+	}
+
+	function testComplexSelector () {
+		$this->assertEquals('#y.cls1.cls2 .cls3 + abc#def[x="y"] > yy, ff',
+			$this->parser->render($this->parser->parse('.cls1.cls2#y .cls3+abc#def[x=y]>yy,ff')));
+	}
+}
